7af25f73bfa009d1d1f66d96a82c2a098b970a6d
ashok.budha2015 <ashok.budha2015@gmail.com>
2020-06-23 21:22:57 +0545

    double image problem solved

--- scrap.py
+++ scrap.py
@@ -8,22 +8,18 @@ url = "https://www.pornpics.com/?q=dani+daniels"
 source_code = requests.get(url)
 
 plain_text = source_code.text
-print(plain_text)
+soup = BeautifulSoup(plain_text)
 
-# soup = BeautifulSoup(plain_text)
-
-# for link in soup.find_all("a",{"class":"rel-link"}):
-#     # href = link.get('href')
-#     # print(href) 
-#     inner_url=link.get('href')
-#     source_code1 = requests.get(inner_url)
-#     plain_text1 = source_code.text
-#     soup1=BeautifulSoup(plain_text1)
-#     for link1 in soup1.find_all("a",{"class":'rel-link'}):
-#         img_name = random.randrange(1,500)
-#         full_name = str(img_name)+ ".jpg"
-#         href=link1.find('img')
-#         shref=href.get('data-src')
-#         print(shref)
-#         # urllib.request.urlretrieve(shref, full_name)
-#     print("loop break")
+for link in soup.find_all("a",{"class":"rel-link"}):
+    inner_url=link.get('href')
+    # print(inner_url)
+    source_code1 = requests.get(inner_url)
+    plain_text1 = source_code1.text
+    soup1=BeautifulSoup(plain_text1)
+    for link1 in soup1.find_all("a",{"class":'rel-link'}):
+        img_name = random.randrange(1,500)
+        full_name = str(img_name)+ ".jpg"
+        href=link1.find('img')
+        shref=href.get('src')
+        urllib.request.urlretrieve(shref, full_name)
+    print("loop break")
